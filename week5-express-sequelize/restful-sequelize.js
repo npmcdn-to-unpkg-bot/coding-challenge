31138ad997d0b0adc33b64add2511cb6cfc8e864
kissmygritts <kissmygritts08@gmail.com>
2016-08-31 11:32:26 -0700

    including restful api...

--- week5-express-sequelize/restful-sequelize.js
+++ week5-express-sequelize/restful-sequelize.js
@@ -7,13 +7,12 @@ var app = express();
 // app configuration
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('view engine', 'ejs');
 
-var sequelize = new Sequelize('testdb', 'mitchellgritts', 'madrush8', {
+// connect to database (user your username and password)
+var sequelize = new Sequelize('testdb', 'username', 'password', {
     dialect: 'postgres'
 });
 
-// database
 var User = sequelize.define('user', {
     first_name: Sequelize.STRING,
     last_name: Sequelize.STRING,
@@ -21,52 +20,32 @@ var User = sequelize.define('user', {
 },{
     underscored: true
 });
-var Project = sequelize.define('project', {
-    project_name: Sequelize.STRING,
-    description: Sequelize.TEXT
-}, {
-    underscored: true
+User.sync().then(function(){
+    User.create({
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email
+    });
 });
-
-User.hasMany(Project);
-Project.belongsTo(User);
-
-sequelize.sync();
-
-// User.create({
-//     first_name: faker.name.firstName(),
-//     last_name: faker.name.lastName(),
-//     email: faker.internet.email()
-// });
-
-// Project.create({
-//     project_name: faker.lorem.words(),
-//     description: faker.lorem.paragraph(),
-//     user_id: 15
-// });
-
-// RESTFULL ROUTES
-// landing
-// app.get('/', function(req, res){
-//     res.json({message: 'landing page'});
-// });
 
 // INDEX - show all users (get)
-app.get('/', function(req, res){
-    res.render('register');
-    // User.findAll().then(function(users){
-    //     res.json(users);
-    // });
+app.get('/users', function(req, res){
+    User.findAll().then(function(users){
+        res.json(users);
+    });
 });
+
 // SHOW - show one user (get)
 app.get('/users/:id', function(req, res){
     User.findById(req.params.id).then(function(user){
         res.json(user);
     });
 });
+
 // CREATE - create new user(post)
 app.post('/users', function(req, res){
     User.create({
+        // data to create a new use in the database
         first_name: req.body.first,
         last_name: req.body.last,
         email: req.body.email
@@ -77,31 +56,22 @@ app.post('/users', function(req, res){
         });
     });
 });
+
 // PUT - update an existing record (update)
 app.put('/users/:id', function(req, res){
     User.update({
+        // data to update to user
         first_name: req.body.first,
         last_name: req.body.last,
         email: req.body.email
     }, {
+        // find the current user in the database to update
         where: {id: req.params.id}
     }).then(function(user){
         res.json({
             message: 'User updated',
             updated: user
         });
-    });
-});
-// DELETE - delete user (delete)
-app.delete('/users/:id', function(req, res){
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(deleted){
-        if(deleted === 1){
-            res.json({message: 'Successfully deleted!'});
-        }
     });
 });
 
